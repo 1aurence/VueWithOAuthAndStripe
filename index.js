acf0f189f6b1aec60ff461cbfdccb58a2df1d8d0
Laurence <laurencehall1@outlook.com>
2019-03-29 19:45:49 -0400

    client added

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ require("./services/passport");
 const cookieSession = require("cookie-session");
 const keys = require("./config/keys");
 const passport = require("passport");
+const cors = require('cors')
+const bodyParser = require('body-parser')
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.use(
   cookieSession({
@@ -11,9 +16,15 @@ app.use(
     keys: [keys.cookieKey]
   })
 );
+
 app.use(passport.initialize()); // Used to initialize passport
 app.use(passport.session()); // Used to persist login sessions
 require("./routes/authRoutes")(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(__dirname + '/public'))
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + './public/index.html'))
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}!`));
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,18 +12,18 @@ module.exports = app => {
     "/auth/google/callback",
     passport.authenticate("google"),
     (req, res) => {
-      res.redirect("/secret");
+      res.redirect("/api/secret");
     }
   );
 
-  app.get("/secret", (req, res) => {
+  app.get("/api/secret", (req, res) => {
     if (req.user) {
       res.json({ msg: "You have reached the secret route", user: req.user });
     } else {
-      res.json({ err: "no user found" });
+      res.json({ err: "No user found" });
     }
   });
-  app.get("/api/logout", (req, res) => {
+  app.get("/auth/logout", (req, res) => {
     req.logout();
     res.redirect("/auth/google");
   });
