688d645ec53b16fc8df3284e289550d2b62401bd
Laurence <laurencehall1@outlook.com>
2019-03-29 19:49:39 -0400

    changed index

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.use(passport.session()); // Used to persist login sessions
 require("./routes/authRoutes")(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(__dirname + './public'))
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + './public/index.html'))
+  app.use(express.static(__dirname + '/public'))
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
 }
 
 const port = process.env.PORT || 3000;
